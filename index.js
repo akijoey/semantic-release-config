385b04bed2b99e1bd032d676bd8c7c226a2b0eea
akijoey <akijoey@akijoey.com>
2021-09-07 10:50:53 +0800

    fix: add plugin

--- index.js
+++ index.js
@@ -3,4 +3,35 @@
 const config = require('./.releaserc')
 config.plugins.pop()
 
+const packageMap = {
+  npm: {
+    plugin: '@semantic-release/npm',
+    assets: 'package.json'
+  },
+  maven: {
+    plugin: 'semantic-release-maven',
+    assets: 'pom.xml'
+  },
+  pypi: {
+    plugin: 'semantic-release-pypi',
+    assets: 'setup.cfg'
+  }
+}
+
+config.releasePackage = name => {
+  if (name !== 'npm') {
+    config.preparePackage(name)
+  }
+  const { plugin } = packageMap[name]
+  config.plugins.push(plugin)
+  return config
+}
+
+config.preparePackage = name => {
+  const { plugin, assets } = packageMap[name]
+  config.plugins[3][1].assets.push(assets)
+  config.prepare.splice(2, 0, plugin)
+  return config
+}
+
 module.exports = config
