cc685f042b1029ed6e7e46bc91ea66779dd9c37f
akijoey <akijoey@akijoey.com>
2021-09-18 15:06:37 +0800

    fix: test function

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,30 @@
 const config = require('../.releaserc')
 
+function getReleasePlugin(plugins) {
+  return plugins[plugins.length - 3]
+}
+
+function getPreparePlugin(prepare) {
+  return prepare[prepare.length - 2]
+}
+
 describe('semantic-release-config', () => {
   it('correct export', () => {
     const index = require('..')
     expect(index).toEqual(config)
+  })
+
+  it('prepare package', () => {
+    const { preparePackage } = require('..')
+    const { prepare } = preparePackage('pypi')
+    const plugin = getPreparePlugin(prepare)
+    expect(plugin).toBe('semantic-release-pypi')
+  })
+
+  it('release package', () => {
+    const { releasePackage } = require('..')
+    const { plugins } = releasePackage('maven')
+    const plugin = getReleasePlugin(plugins)
+    expect(plugin).toBe('semantic-release-maven')
   })
 })
